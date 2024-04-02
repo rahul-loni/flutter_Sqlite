737f4e737ce382dce043f2c0d97265f9ac5b0a3f
Rahul Kumar <rahul@gmail.com>
2024-04-02 17:24:31 +0545

    second commit

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sql/sqlHelper.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,126 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> _journals = [];
+  bool _isLoading = true;
+  void _refreshJaurnals() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _journals = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshJaurnals();
+    print("..number of items ${_journals.length}");
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  Future<void> _addItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
+    _refreshJaurnals();
+  }
+
+  Future<void> _updateItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
+    _refreshJaurnals();
+  }
+
+  void _showForm(int? id) async {
+    if (id != null) {
+      final existingJournal =
+          _journals.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingJournal['title'];
+      _descriptionController.text = existingJournal['description'];
+    }
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (_) => Container(
+              padding: EdgeInsets.only(
+                top: 15,
+                left: 15,
+                right: 15,
+                bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(hintText: 'Title'),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(hintText: 'Description'),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (id == null) {
+                        await _addItem();
+                      }
+                      if (id != null) {
+                        //await _updateItem(id);
+                      }
+                      _titleController.text = "";
+                      _descriptionController.text = "";
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      id == null ? 'Create New ' : 'update item',
+                    ),
+                  ),
+                ],
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Curd Operation"),
+      ),
+      body: ListView.builder(
+          itemCount: _journals.length,
+          itemBuilder: (context, index) => Card(
+                color: Color.fromARGB(255, 181, 180, 179),
+                margin: const EdgeInsets.all(15),
+                child: ListTile(
+                  title: Text(_journals[index]['title']),
+                  subtitle: Text(_journals[index]['description']),
+                  trailing: SizedBox(
+                    width: 100,
+                    child: Row(
+                      children: [
+                        IconButton(
+                            onPressed: () => _showForm(_journals[index]['id']),
+                            icon: const Icon(Icons.edit)),
+                        IconButton(
+                            onPressed: () => null,
+                            icon: const Icon(Icons.delete)),
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _showForm(null),
+      ),
+    );
   }
 }
